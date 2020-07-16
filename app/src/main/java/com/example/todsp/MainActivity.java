bd278868fa2a159c1f17d0575c7e501c9b0424e8
Devin1996 <devinchandula@gmail.com>
2020-07-16 06:11:38 +0530

    Created up to Main menu activity

--- app/src/main/java/com/example/todsp/MainActivity.java
+++ app/src/main/java/com/example/todsp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.todsp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button driverButton, conductorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        driverButton = (Button) findViewById(R.id.driver_btn);
+        conductorButton = (Button) findViewById(R.id.conductor_btn);
+
+        conductorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , ConductorLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , DriverLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/todsp/SplashActivity.java
+++ app/src/main/java/com/example/todsp/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.todsp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,28 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run(){
+                try{
+                    sleep(5000);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    Intent WelcomeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(WelcomeIntent);
+                }
+            }
+
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
