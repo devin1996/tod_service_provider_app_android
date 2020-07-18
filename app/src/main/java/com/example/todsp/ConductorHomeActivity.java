00f0fb94711b38939a5da4d4ec2041af8432f394
Devin1996 <devinchandula@gmail.com>
2020-07-18 15:15:06 +0530

    Log In completed in both accounts

---
+++ app/src/main/java/com/example/todsp/ConductorHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.todsp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConductorHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conductor_home);
+    }
+}
--- app/src/main/java/com/example/todsp/ConductorLoginActivity.java
+++ app/src/main/java/com/example/todsp/ConductorLoginActivity.java
@@ -1,14 +1,150 @@
 package com.example.todsp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.todsp.Model.Users;
+import com.example.todsp.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
 
 public class ConductorLoginActivity extends AppCompatActivity {
+
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+    private TextView ForgetPasswordLink;
+
+    private CheckBox chkBoxRememberMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conductor_login);
+
+        LoginButton = (Button) findViewById(R.id.con_login_btn);
+        InputPassword = (EditText) findViewById(R.id.con_login_pwd);
+        InputPhoneNumber = (EditText) findViewById(R.id.con_login_no);
+        ForgetPasswordLink = findViewById(R.id.forget_password_link_con);
+        loadingBar = new ProgressDialog(this);
+
+
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.con_remember_me);
+        Paper.init(this);
+
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                LoginUser();
+            }
+        });
+
+        ForgetPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ConductorLoginActivity.this, ResetPasswordActivity.class);
+                intent.putExtra("check", "login");
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void LoginUser() {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Please enter your Phone Number", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter your Password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait while Checking credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(final String phone, final String password) {
+
+        if(chkBoxRememberMe.isChecked())
+        {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
+        }
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child("User").child("Conductor").child(phone).exists())
+                {
+                    Users usersData = dataSnapshot.child("User").child("Conductor").child(phone).getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone)){
+
+                        if (usersData.getPassword().equals(password)){
+
+
+                            Toast.makeText(ConductorLoginActivity.this, "You have Logged in Successfully...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(ConductorLoginActivity.this, ConductorHomeActivity.class);
+                            Prevalent.currentOnlineUser = usersData;
+                            startActivity(intent);
+
+
+
+                        }
+
+                        else {
+                            loadingBar.dismiss();
+                            Toast.makeText(ConductorLoginActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+                else
+                {
+                    Toast.makeText(ConductorLoginActivity.this, "Account with this " +phone+ "number do not exists", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
